911b1ce0549a7d222347a988e3504784617c874e
Thibstars <thibault.helsmoortel@gmail.com>
2024-05-22 16:56:30 +0200

    Return key if i18n or message is falsy

--- extension/utils.js
+++ extension/utils.js
@@ -1,7 +1,12 @@
 // Use this method to avoid inspection warnings on i18n's getMessage method
 function getMessage(i18n, key) {
-  // noinspection JSUnresolvedReference
-  return i18n.getMessage(key);
+  let result = key;
+  if (i18n) {
+    // noinspection JSUnresolvedReference
+    result = i18n.getMessage(key) ? i18n.getMessage(key) : key;
+  }
+
+  return result;
 }
 
 // Use this method to avoid inspection warnings on chrome's runtime.getManifest method
